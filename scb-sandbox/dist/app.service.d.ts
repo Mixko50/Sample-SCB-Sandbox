f5b90489ac83be70cc6444291a183919afa3cdeb
Apisit Maneerat <70818990+Mixko50@users.noreply.github.com>
2021-11-06 23:59:00 +0700

    Delete scb-sandbox/dist directory

--- scb-sandbox/dist/app.service.d.ts
+++
@@ -1,5 +0,0 @@
-import RefreshToken from './interfaces/refreshToken';
-export declare class AppService {
-    getHello(): string;
-    refreshToken(accessToken: string): Promise<RefreshToken>;
-}
--- scb-sandbox/dist/interfaces/refreshToken.d.ts
+++
@@ -1,4 +0,0 @@
-export default interface RefreshToken {
-    accessToken: string;
-    expiresAt?: bigint;
-}
